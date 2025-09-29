73c6b662b1d65e0178980d3191ec4a825804c203
codecommit+1-at-437146165873 <6s+Q6uqCQAenI63c1A0LIpmyFg8oVqPdIAKaRmP8qMI=>
2025-09-30 01:54:16 +0200

    feat(deployment): add frontend build step to prod deployment script

--- infra/deploy-prod.sh
+++ infra/deploy-prod.sh
@@ -10,6 +10,11 @@ export REGION="us-central1" # Or your preferred region
 export REPO_NAME="network-topology-repo"
 export IMAGE_NAME="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${SERVICE_NAME}"
 
+# Build the frontend
+echo "Building the frontend..."
+npm run frontend:build
+echo "Frontend build complete."
+
 # Build the container image using Cloud Build
 gcloud builds submit --tag $IMAGE_NAME --project $PROJECT_ID
 
